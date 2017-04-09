a132d48315dda2a020e97165669b87b2cf74645e
IT Trouble Shooter <troubles.maker.shooter@gmail.com>
2017-04-09 16:16:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/toolbarexample/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.toolbarexample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+TextView txtWebLink;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtWebLink = (TextView) findViewById(R.id.txtWebLink);
+
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        // Display icon in the toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
+
+
+    }
+
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    public void displayWeb(View v){
+
+        openWebPage("http://uwolnijcialo.pl");
+    }
+}
